18bd611b238935d81fbc54fa7cc123c0c7668001
iT Gurus Software <itgurussoftware@hjw139.local>
2017-12-22 11:18:31 +0530

    Initial Commit

---
+++ Speedometer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Speedometer
+//
+//  Created by iT Gurus Software on 22/12/17.
+//  Copyright © 2017 iT Gurus Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
